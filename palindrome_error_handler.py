a66816fa28ddf6b320a24377a613c12a8369378f
ashgharibyan <ashghcode@gmail.com>
2025-05-08 20:24:23 -0700

    Added main class / wrapper for error handling #7

---
+++ palindrome_error_handler.py
@@ -0,0 +1,29 @@
+from tkinter import messagebox
+
+
+class ErrorHandler:
+    @staticmethod
+    def validate_input(word: str) -> bool:
+        """
+        Validate the input word to ensure it is a palindrome.
+        """
+        return bool
+
+    @staticmethod
+    def show_error(title: str, message: str):
+        """
+        Show an error message box.
+        """
+
+    @staticmethod
+    def show_info(title: str, message: str):
+        """
+        Show an information message box.
+        """
+    
+    @staticmethod
+    def show_failure(title: str, message: str):
+        """
+        Show a failure message box.
+        """
+        
